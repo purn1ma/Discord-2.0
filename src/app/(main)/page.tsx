6239dcf6d2f6d238c33e7a9ca7a5c1e10c5cd9ff
Purnima Shrivastava <anuragshrivastava@Anurags-MacBook-Air.local>
2023-11-20 04:13:06 +0530

    added dark mode

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { ModeToggle } from '@/components/ModeToggle'
+import { UserButton } from '@clerk/nextjs'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button isLoading={true}>Add</Button>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl='/'/>
+      <ModeToggle/>
+    </div>
   )
 }
